0929f7123baba508f47026a98d5678ec9b490dd1
kastenbauer27 <pkast2016@comcast.net>
2020-12-11 22:31:06 -0600

    used redux to display input values on review page

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class ReviewPage extends Component {
     state = {  }
     render() { 
-        return ( 
-            <p>Review Page</p>
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Feeling</th>
+                        <th>Understanding</th>
+                        <th>Support</th>
+                        <th>Comments</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{this.props.reduxState.feelingReducer}</td>
+                        <td>{this.props.reduxState.comprehensionReducer}</td>
+                        <td>{this.props.reduxState.supportReducer}</td>
+                        <td>{this.props.reduxState.commentsReducer}</td>
+                    </tr>
+                </tbody>
+            </table>
+            
          );
     }
 }
+
+const mapStateToProps = (reduxState) => ({
+    reduxState
+})
  
-export default ReviewPage;
+export default connect(mapStateToProps)(ReviewPage);
